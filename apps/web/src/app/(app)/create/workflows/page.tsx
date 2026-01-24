5286d535507dce41f2f4df8dd94fc4fe5958db2f
Omar Mamon <109483886+Spafic@users.noreply.github.com>
2026-01-24 23:27:38 +0200

    Update apps/web/src/app/(app)/create/workflows/page.tsx
    Co-authored-by: Shehab <127568346+ashmod@users.noreply.github.com>

--- apps/web/src/app/(app)/create/workflows/page.tsx
+++ apps/web/src/app/(app)/create/workflows/page.tsx
@@ -28,6 +28,7 @@ export default function CreateWorkflowsPage() {
 
   useEffect(() => {
     void actions.init();
+    // eslint-disable-next-line react-hooks/exhaustive-deps 
   }, []);
 
   return (
