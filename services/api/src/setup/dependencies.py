1d2da21cedf3d4465cc5f8285fb67787e44b7141
Youssef Shahean <109446360+unauthorised-401@users.noreply.github.com>
2026-03-14 02:21:37 +0200

    refactor(api): remove unnecessary async from dependency functions (#436)

--- services/api/src/setup/dependencies.py
+++ services/api/src/setup/dependencies.py
@@ -2,6 +2,6 @@ from src.core.dependencies import DatabaseDep
 from src.setup.service import SetupService
 
 
-async def get_setup_service(db: DatabaseDep) -> SetupService:
+def get_setup_service(db: DatabaseDep) -> SetupService:
     """Dependency to get setup service instance."""
     return SetupService(db=db)
