f7f807eec310c90ac2e74deb9f5929d7d540ae54
ashmod <shehabmahmoud2003@gmail.com>
2026-05-04 22:19:26 +0300

    formatting

--- services/api/migrations/versions/2026_04_28_2155_fb62ad1bfffc_add_gemini_enum.py
+++ services/api/migrations/versions/2026_04_28_2155_fb62ad1bfffc_add_gemini_enum.py
@@ -4,13 +4,14 @@ Revision ID: fb62ad1bfffc
 Revises: f1a2b3c4d5e6
 Create Date: 2026-04-28 21:55:08.948928
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 
 # Revision identifiers
-revision: str = 'fb62ad1bfffc'
-down_revision: Union[str, None] = 'f1a2b3c4d5e6'
+revision: str = "fb62ad1bfffc"
+down_revision: Union[str, None] = "f1a2b3c4d5e6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
